5fe76ce39d055847d63cd0e4a46e04046e0bb3e0
tonylindorock <tonylindorock@gmail.com>
2022-02-17 17:57:27 -0500

    P2: Made food prefabs

---
+++ Prototype_2/Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    public float waitTime = 1f;
+
+    float timeLeft = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = waitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0f){
+            Destroy(this.gameObject);
+        }
+    }
+}
